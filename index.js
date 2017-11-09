f7af9cf1cf1848e0010755e470ab426a3c8b9720
Chely Ho <adept_inept@yahoo.com>
2017-11-09 21:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function BoardMember(name, homeState, training){
 
   this.veto = function(){
     return "No, I must disagree";
-  }
+  };
   this.approve = function(){
     return "You can do that!";
   };
